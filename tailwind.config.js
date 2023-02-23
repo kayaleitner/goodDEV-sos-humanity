0f0ee70538556726e6c233606d8de8182ae7f67f
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-02-23 17:12:23 +0100

    base styling + started working on theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,16 @@ module.exports = {
     colors: {
       white: '#fff',
       black: '#000',
-      blue: '#1113ff',
+      green: '#169b83',
+      grey: '#7b838d',
       current: 'currentColor'
     },
     screens: {
       sm: '641px',
       md: '769px',
       lg: '1181px',
-      xl: '1281px'
+      xl: '1281px',
+      max: '1440px'
     },
     extend: {
       borderWidth: {
@@ -28,7 +30,8 @@ module.exports = {
       },
       spacing: {
         sm: '15px',
-        lg: '35px'
+        md: '35px',
+        lg: '50px'
       }
     }
   },
