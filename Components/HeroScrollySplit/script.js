49cae1471de85ddd2fe1a5b7e57a59a937fa7425
Arne Spremberg <mail@arnespremberg.com>
2023-02-23 19:52:49 +0100

    new build

--- Components/HeroScrollySplit/script.js
+++ Components/HeroScrollySplit/script.js
@@ -2,12 +2,11 @@ import Swiper, { Navigation, A11y, Autoplay } from 'swiper'
 import 'swiper/css/bundle'
 import { buildRefs, getJSON } from '@/assets/scripts/helpers.js'
 import gsap from 'gsap'
-import ScrollTrigger from 'gsap/ScrollTrigger'
 
 export default function (heroScrollySplit) {
   const refs = buildRefs(heroScrollySplit, true)
   const data = getJSON(heroScrollySplit)
-  let timeline = gsap.timeline({
+  const timeline = gsap.timeline({
     scrollTrigger: {
       trigger: refs.section,
       pin: true,
@@ -18,41 +17,41 @@ export default function (heroScrollySplit) {
       // markers: true,
       snap: {
         snapTo: 1,
-        duration: {min: 0.01, max: 0.3},
+        duration: { min: 0.01, max: 0.3 },
         delay: 0,
-        ease: "power0.inOut",
+        ease: 'power0.inOut',
         inertia: true
       }
     }
-  });
+  })
   timeline
     .from(refs.text_2, {
       opacity: 1,
-      duration: 0.2,
+      duration: 0.2
     })
     .to(refs.left, {
-      y: '100vh',
+      y: '100vh'
     }, 0)
     .to(refs.right, {
-      y: '-100vh',
+      y: '-100vh'
     }, 0)
     .to(refs.text_2, {
       opacity: 0,
-      duration: 0.2,
+      duration: 0.2
     }, 0)
     .to(refs.text_3, {
       opacity: 1,
-      duration: 0.2,
+      duration: 0.2
     }, 0)
     .to(refs.text_1, {
       opacity: 1,
-      duration: 0.2,
+      duration: 0.2
     }, 0)
   const swiper = initSlider(refs, data)
   return () => swiper.destroy()
 }
 
-function initSlider(refs, data) {
+function initSlider (refs, data) {
   const { options } = data
   const config = {
     modules: [Navigation, A11y, Autoplay],
