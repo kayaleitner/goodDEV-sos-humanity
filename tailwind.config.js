60bf3715e55e75ae526d39b513857a5f0fa95625
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-02-24 11:35:48 +0100

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,53 @@ module.exports = {
     './Components/**/*.{php,twig}'
   ],
   theme: {
+    borderWidth: {
+      DEFAULT: '1px',
+      0: '0',
+      2: '2px',
+      3: '3px'
+    },
     colors: {
       white: '#fff',
       black: '#000',
-      green: '#169b83',
+      navygreen: '#169b83',
+      blue: '#272a5f',
+      green: '#52b756',
       grey: '#7b838d',
       current: 'currentColor'
+    },
+    fontSize: {
+      btn: ['1.25rem', {
+        lineHeight: '1.25rem',
+        fontWeight: '400'
+      }],
+      h1: ['4.813rem', {
+        lineHeight: '5.25rem',
+        // letterSpacing: '-0.01em',
+        fontWeight: '400'
+      }],
+      h2: ['3.438rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700'
+      }],
+      h3: ['1.875rem', {
+        lineHeight: '2.25rem',
+        letterSpacing: '-0.02em',
+        fontWeight: '700'
+      }],
+      h4: ['2rem', {
+        lineHeight: 'auto',
+        fontWeight: '700'
+      }],
+      h5: ['1.563rem', {
+        lineHeight: 'auto',
+        fontWeight: '400'
+      }],
+      h6: ['1.25rem', {
+        lineHeight: 'auto',
+        fontWeight: '400'
+      }]
     },
     screens: {
       sm: '641px',
@@ -29,9 +70,12 @@ module.exports = {
         4: '4px'
       },
       spacing: {
+        xs: '10px',
         sm: '15px',
         md: '35px',
-        lg: '50px'
+        lg: '50px',
+        xl: '75px',
+        xxl: '100px'
       }
     }
   },
