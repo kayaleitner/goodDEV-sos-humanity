fe37937ab455ec77b0c370a255dae6fb718d52d1
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-03-01 13:02:09 +0100

    removed unused component and function for theming

--- inc/theming.php
+++ inc/theming.php
@@ -2,21 +2,21 @@
 
 namespace Flynt\Theming;
 
-use Flynt\Utils\Options;
+// use Flynt\Utils\Options;
 
-Options::addGlobal('GlobalTheme', [
-    [
-        'label' => __('Theme', 'flynt'),
-        'name' => 'globalTheme',
-        'type' => 'select',
-        'allow_null' => 0,
-        'multiple' => 0,
-        'ui' => 0,
-        'ajax' => 0,
-        'choices' => [
-            'cbe' => __('CrossBoundary Energy', 'flynt'),
-            'cbg' => __('CrossBoundary Group', 'flynt'),
-        ],
-        'default_value' => 'cbe',
-    ]
-]);
+// Options::addGlobal('GlobalTheme', [
+//     [
+//         'label' => __('Theme', 'flynt'),
+//         'name' => 'globalTheme',
+//         'type' => 'select',
+//         'allow_null' => 0,
+//         'multiple' => 0,
+//         'ui' => 0,
+//         'ajax' => 0,
+//         'choices' => [
+//             'cbe' => __('CrossBoundary Energy', 'flynt'),
+//             'cbg' => __('CrossBoundary Group', 'flynt'),
+//         ],
+//         'default_value' => 'cbe',
+//     ]
+// ]);
