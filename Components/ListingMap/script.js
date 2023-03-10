fb7d83d9f365c45b7c20490201d7cd3ee3a13c45
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-03-10 12:24:52 +0100

    loosely started ListingMap (wip)

---
+++ Components/ListingMap/script.js
@@ -0,0 +1,24 @@
+// const fs = require('fs');
+// const DottedMap = require('dotted-map').default;
+
+// const map = new DottedMap({ height: 60, grid: 'diagonal' });
+
+// map.addPin({
+//   lat: 40.73061,
+//   lng: -73.935242,
+//   svgOptions: { color: '#d6ff79', radius: 0.4 },
+// });
+// map.addPin({
+//   lat: 48.8534,
+//   lng: 2.3488,
+//   svgOptions: { color: '#fffcf2', radius: 0.4 },
+// });
+
+// const svgMap = map.getSVG({
+//   radius: 0.22,
+//   color: '#423B38',
+//   shape: 'circle',
+//   backgroundColor: '#020300',
+// });
+
+// fs.writeFileSync('./map.svg', svgMap);
