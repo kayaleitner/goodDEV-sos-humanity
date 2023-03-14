615819cf6d973bfd259891ba7b2506a8e814be8f
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-03-14 15:15:45 +0100

    update page template

--- inc/fieldGroups/pageComponents.php
+++ inc/fieldGroups/pageComponents.php
@@ -5,6 +5,62 @@ use Flynt\Components;
 
 add_action('Flynt/afterRegisterComponents', function () {
     ACFComposer::registerFieldGroup([
+        'name' => 'pageMeta',
+        'title' => 'Page Options',
+        'style' => '',
+        'menu_order' => 1,
+        'position' => 'side',
+        'fields' => [
+            [
+                'label' => __('Page Header', 'flynt'),
+                'instructions' => __('Display page header. When disabled, you can add another block acting as header', 'flynt'),
+                'name' => 'displayBanner',
+                'type' => 'true_false',
+                'ui' => 1,
+                'ui_on_text' => 'Yes',
+                'ui_off_text' => 'No',
+                'default_value' => 1,
+                'wrapper' => [
+                    'width' => 100,
+                ]
+            ],
+            [
+                'label' => __('Intro', 'flynt'),
+                'instructions' => __('Maximum 200 characters (~ 30 words).', 'flynt'),
+                'name' => 'intro',
+                'type' => 'textarea',
+                'maxlength' => 200,
+                'wrapper' => [
+                    'width' => 100
+                ]
+            ],
+        ],
+        'location' => [
+            [
+                // [
+                //     'param' => 'post_type',
+                //     'operator' => '==',
+                //     'value' => 'page'
+                // ],
+                [
+                    'param' => 'post_type',
+                    'operator' => '!=',
+                    'value' => 'post'
+                ],
+                [
+                    'param' => 'post_type',
+                    'operator' => '!=',
+                    'value' => 'project'
+                ],
+                [
+                    'param' => 'post_type',
+                    'operator' => '!=',
+                    'value' => 'reusable-components'
+                ],
+            ],
+        ],
+    ]);
+    ACFComposer::registerFieldGroup([
         'name' => 'pageComponents',
         'title' => __('Page Components', 'flynt'),
         'style' => 'seamless',
