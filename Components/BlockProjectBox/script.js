894b3f7fff8c8ba134290109fb0177fb1854c485
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-03-14 15:16:52 +0100

    add project box block

---
+++ Components/BlockProjectBox/script.js
@@ -0,0 +1,37 @@
+import DottedMap from 'dotted-map'
+import { buildRefs } from '@/assets/scripts/helpers.js'
+
+const resolution = 10
+
+export default function (projectMap) {
+  const refs = buildRefs(projectMap)
+  const wrapper = refs.mapWrapper
+
+  const map = new DottedMap({
+    height: wrapper.offsetHeight / resolution,
+    width: wrapper.offsetWidth / resolution,
+    grid: 'vertical'
+  })
+
+  const svgMap = map.getSVG({
+    radius: 0.3,
+    color: '#7b838d',
+    shape: 'circle'
+  })
+
+  document.getElementById('dottedMap').innerHTML = svgMap
+
+  const pinRefs = document.querySelectorAll('[data-ref="pin"]')
+  console.log(pinRefs)
+
+  pinRefs.forEach(pin => {
+    const { latitude, longitude } = pin.dataset
+    console.log(latitude, longitude)
+    const pinCoords = map.getPin({
+      lat: parseFloat(latitude),
+      lng: parseFloat(longitude)
+    })
+    pin.style.left = pinCoords.x * resolution + 'px'
+    pin.style.top = pinCoords.y * resolution + 'px'
+  })
+}
