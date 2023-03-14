dc2134852791f2602df27fe338c5e7657915b813
Arne Spremberg <mail@arnespremberg.com>
2023-03-13 23:44:16 -0400

    re-introduce block settings for nav main

--- Components/NavigationMain/functions.php
+++ Components/NavigationMain/functions.php
@@ -6,6 +6,21 @@ use Flynt\Utils\Asset;
 use Flynt\Utils\Options;
 use Timber\Timber;
 
+add_filter('Flynt/addComponentData?name=NavigationMain', function ($data) {
+    $data['menu'] = Timber::get_menu('navigation_main') ?? Timber::get_pages_menu();
+    $data['logo'] = [
+        'src' => get_theme_mod('custom_header_logo') ? get_theme_mod('custom_header_logo') : Asset::requireUrl('assets/images/logo.svg'),
+        'alt' => get_bloginfo('name')
+    ];
+    $data['logo_dark'] = [
+        'src' => get_theme_mod('logo_dark') ? get_theme_mod('logo_dark') : Asset::requireUrl('assets/images/logo.svg'),
+        'alt' => get_bloginfo('name')
+    ];
+
+    return $data;
+});
+
+
 add_action('init', function () {
     register_nav_menus([
         'navigation_main' => __('Navigation Main', 'flynt')
@@ -27,16 +42,45 @@ add_action('customize_register', function ($wp_customize) {
 });
 
 
-add_filter('Flynt/addComponentData?name=NavigationMain', function ($data) {
-    $data['menu'] = Timber::get_menu('navigation_main') ?? Timber::get_pages_menu();
-    $data['logo'] = [
-        'src' => get_theme_mod('custom_header_logo') ? get_theme_mod('custom_header_logo') : Asset::requireUrl('assets/images/logo.svg'),
-        'alt' => get_bloginfo('name')
-    ];
-    $data['logo_dark'] = [
-        'src' => get_theme_mod('logo_dark') ? get_theme_mod('logo_dark') : Asset::requireUrl('assets/images/logo.svg'),
-        'alt' => get_bloginfo('name')
-    ];
-
-    return $data;
-});
+Options::addTranslatable('NavigationMain', [
+    [
+        'label' => __('Call to Action', 'flynt'),
+        'name' => 'ctaTab',
+        'type' => 'tab',
+        'placement' => 'top',
+        'endpoint' => 0
+    ],
+    [
+        'label' => __('CTA Link', 'flynt'),
+        'name' => 'ctaLink',
+        'type' => 'link',
+        'return_format' => 'array',
+        'wrapper' =>  [
+            'width' => '100',
+        ]
+    ],
+    [
+        'label' => __('Labels', 'flynt'),
+        'name' => 'labelsTab',
+        'type' => 'tab',
+        'placement' => 'top',
+        'endpoint' => 0
+    ],
+    [
+        'label' => '',
+        'name' => 'labels',
+        'type' => 'group',
+        'sub_fields' => [
+            [
+                'label' => __('Aria Label', 'flynt'),
+                'name' => 'ariaLabel',
+                'type' => 'text',
+                'default_value' => __('Main', 'flynt'),
+                'required' => 1,
+                'wrapper' => [
+                    'width' => '50',
+                ],
+            ],
+        ],
+    ],
+]);
