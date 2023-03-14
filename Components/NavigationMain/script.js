baaab035df6f1317db0e5eb94fb83a2845b43d9e
Arne Spremberg <mail@arnespremberg.com>
2023-03-14 00:33:27 -0400

    desktop menu scrolling system

--- Components/NavigationMain/script.js
+++ Components/NavigationMain/script.js
@@ -9,18 +9,39 @@ export default function (el) {
 
   onBreakpointChange()
 
-  function onBreakpointChange () {
+  function onBreakpointChange() {
     if (isDesktopMediaQuery.matches) {
       setScrollPaddingTop()
     }
   }
 
-  function setScrollPaddingTop () {
+  function setScrollPaddingTop() {
     const scrollPaddingTop = document.getElementById('wpadminbar')
       ? navigationHeight + document.getElementById('wpadminbar').offsetHeight
       : navigationHeight
     document.documentElement.style.scrollPaddingTop = `${scrollPaddingTop}px`
   }
+
+  var sections = gsap.utils.toArray('#mainContent flynt-component');
+  sections.forEach((section) => {
+    gsap.to(section, {
+      scrollTrigger: {
+        trigger: section,
+        start: 'top top+=100',
+        start: "top top",
+        end: "bottom top+=70",  
+        onToggle: (self) => {
+          if (self.isActive) {
+            el.classList.remove('bg-white/40', 'backdrop-blur-md', 'text-grey', 'text-white')
+            el.querySelectorAll('.logo').forEach((logo) => logo.classList.remove('flex', 'hidden'))
+            self.trigger.dataset.navstyle === 'dark' ? el.querySelector('.logo_dark').classList.add('hidden') : el.querySelector('.logo_dark').classList.add('flex');
+            self.trigger.dataset.navstyle !== 'dark' ? el.querySelector('.logo_light').classList.add('hidden') : el.querySelector('.logo_light').classList.add('flex');
+            self.trigger.dataset.navstyle === 'dark' ? el.classList.add('text-white') : el.classList.add('bg-white/40', 'backdrop-blur-md', 'text-grey');
+          }
+        }
+      }
+    });
+  })
 
   // hide/show navigation on scroll
   const showAnim = gsap.from('[name="NavigationMain"]', {
