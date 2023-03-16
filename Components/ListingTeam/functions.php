4df977c601f7067cb5ab2ba11a22ef4dba390270
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-03-16 17:55:11 +0100

    add listing team block

---
+++ Components/ListingTeam/functions.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Flynt\Components\ListingTeam;
+
+use Flynt\FieldVariables;
+use Flynt\Utils\Options;
+use Timber\Timber;
+
+const POST_TYPE = 'team';
+
+add_filter('Flynt/addComponentData?name=ListingTeam', function ($data) {
+    $postType = POST_TYPE;
+
+    $data['taxonomies'] = $data['taxonomies'] ?: [];
+
+    $data['posts'] = Timber::get_posts([
+        'post_status' => 'publish',
+        'post_type' => $postType,
+        'category' => join(',', array_map(function ($taxonomy) {
+            return $taxonomy->term_id;
+        }, $data['taxonomies'])),
+        'posts_per_page' => $data['options']['maxPosts'],
+        'ignore_sticky_posts' => 1,
+        'post__not_in' => array(get_the_ID())
+    ]);
+
+    $data['postTypeArchiveLink'] = get_post_type_archive_link($postType);
+
+    return $data;
+});
+
+function getACFLayout()
+{
+    return [
+        'name' => 'ListingTeam',
+        'label' => __('Listing: Team', 'flynt'),
+        'sub_fields' => [
+            [
+                'label' => __('General', 'flynt'),
+                'name' => 'generalTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => __('Title', 'flynt'),
+                'instructions' => __('Want to add a headline? And a paragraph? Go ahead! Or just leave it empty and nothing will be shown.', 'flynt'),
+                'name' => 'blockTitle',
+                'type' => 'text',
+            ],
+            [
+                'label' => __('Categories', 'flynt'),
+                'instructions' => __('Select 1 or more categories or leave empty to show from all posts.', 'flynt'),
+                'name' => 'taxonomies',
+                'type' => 'taxonomy',
+                'taxonomy' => 'category',
+                'field_type' => 'multi_select',
+                'allow_null' => 1,
+                'multiple' => 1,
+                'add_term' => 0,
+                'save_terms' => 0,
+                'load_terms' => 0,
+                'return_format' => 'object'
+            ],
+            [
+                'label' => __('Options', 'flynt'),
+                'name' => 'optionsTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => '',
+                'name' => 'options',
+                'type' => 'group',
+                'layout' => 'row',
+                'sub_fields' => [
+                    FieldVariables\getTheme(),
+                    [
+                        'label' => __('Max Columns', 'flynt'),
+                        'name' => 'maxPosts',
+                        'type' => 'number',
+                        'default_value' => 3,
+                        'min' => 1,
+                        // 'max' => 4,
+                        'step' => 1
+                    ]
+                ]
+            ],
+        ]
+    ];
+}
+
+Options::addTranslatable('ListingTeam', [
+    [
+        'label' => __('Labels', 'flynt'),
+        'name' => 'labelsTab',
+        'type' => 'tab',
+        'placement' => 'top',
+        'endpoint' => 0
+    ],
+    [
+        'label' => '',
+        'name' => 'labels',
+        'type' => 'group',
+        'sub_fields' => [
+            [
+                'label' => __('All Posts', 'flynt'),
+                'name' => 'allPosts',
+                'type' => 'text',
+                'default_value' => __('View all', 'flynt'),
+                'required' => 1,
+                'wrapper' => [
+                    'width' => 50
+                ],
+            ],
+            [
+                'label' => __('All Posts Link', 'flynt'),
+                'name' => 'allPostsLink',
+                'type' => 'link',
+                'required' => 1,
+                'wrapper' => [
+                    'width' => 50
+                ],
+            ],
+            [
+                'label' => __('Read More', 'flynt'),
+                'name' => 'readMore',
+                'type' => 'text',
+                'default_value' => __('Read More', 'flynt'),
+                'required' => 1,
+                'wrapper' => [
+                    'width' => 50
+                ],
+            ],
+            [
+                'label' => __('Reading Time - (20) min read', 'flynt'),
+                'instructions' => __('%d is placeholder for number of minutes', 'flynt'),
+                'name' => 'readingTime',
+                'type' => 'text',
+                'default_value' => __('%d min read', 'flynt'),
+                'required' => 1,
+                'wrapper' => [
+                    'width' => 50
+                ],
+            ]
+        ],
+    ]
+]);
