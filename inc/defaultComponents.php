d934d458db37f9e2fd5013872801b36775a35827
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-03-22 14:39:46 +0100

    add default components function (wip)

---
+++ inc/defaultComponents.php
@@ -0,0 +1,15 @@
+<?php
+
+add_filter('acf/load_value/name=projectComponents', 'projects_default_components', 10, 3);
+function projects_default_components($value, $post_id, $field)
+{
+    if ($value !== null) {
+        return $value;
+    }
+    $value = array(
+        array(
+            'acf_fc_layout' => 'BlockWysiwyg',
+        )
+    );
+    return $value;
+}
