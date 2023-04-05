d75103b77557100b95c37c46f05ff7bc2c9d27dc
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-04-05 17:09:21 +0200

    update colouring system

--- Components/BlockNewsletter/functions.php
+++ Components/BlockNewsletter/functions.php
@@ -6,6 +6,7 @@ use Flynt\Utils\Asset;
 use Flynt\Utils\Options;
 use Flynt\Shortcodes;
 use Flynt\ComponentManager;
+use Flynt\FieldVariables;
 use Timber\Timber;
 
 // function getACFLayout()
@@ -42,12 +43,22 @@ use Timber\Timber;
 
 Options::addTranslatable('BlockNewsletter', [
     [
+        'label' => __('General', 'flynt'),
+        'name' => 'generalTab',
+        'type' => 'tab',
+        'placement' => 'top',
+        'endpoint' => 0
+    ],
+    [
         'label' => __('Content', 'flynt'),
         'name' => 'contentHtml',
         'type' => 'wysiwyg',
         'delay' => 1,
         'media_upload' => 0,
         'required' => 0,
+        'wrapper' => [
+            'width' => 50,
+        ]
     ],
     [
         'label' => __('Contact Form', 'flynt'),
@@ -56,5 +67,26 @@ Options::addTranslatable('BlockNewsletter', [
         'delay' => 1,
         'media_upload' => 0,
         'required' => 0,
+        'wrapper' => [
+            'width' => 50,
+        ]
     ],
+    [
+        'label' => __('Options', 'flynt'),
+        'name' => 'optionsTab',
+        'type' => 'tab',
+        'placement' => 'top',
+        'endpoint' => 0
+    ],
+    [
+        'label' => '',
+        'name' => 'options',
+        'type' => 'group',
+        'layout' => 'row',
+        'sub_fields' => [
+            // FieldVariables\getTheme(),
+            FieldVariables\getColorBackground(),
+            FieldVariables\getNavStyle('dark-blur'),
+        ]
+    ]
 ]);
