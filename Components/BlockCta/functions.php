6f2455a2ab3bcbca91753146acf22774c6aed766
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-04-11 12:40:13 +0200

    add cta block instead of using fixed blocks for ease of use

---
+++ Components/BlockCta/functions.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Flynt\Components\BlockCta;
+
+use Flynt\FieldVariables;
+
+function getACFLayout()
+{
+    return [
+        'name' => 'BlockCta',
+        'label' => __('CTA Buy', 'flynt'),
+        'sub_fields' => [
+            [
+                'label' => __('General', 'flynt'),
+                'name' => 'generalTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0,
+            ],
+            [
+                'label' => __('Content', 'flynt'),
+                'name' => 'contentHtml',
+                'type' => 'wysiwyg',
+                'tabs' => 'visual',
+                'delay' => 1,
+                'media_upload' => 0,
+                'required' => 0,
+            ],
+            [
+                'label' => __('Button', 'flynt'),
+                'name' => 'buttonLink',
+                'type' => 'link',
+                'required' => 0,
+                'wrapper' => [
+                    'width' => 100
+                ],
+            ],
+            [
+                'label' => __('Options', 'flynt'),
+                'name' => 'optionsTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => '',
+                'name' => 'options',
+                'type' => 'group',
+                'layout' => 'row',
+                'sub_fields' => [
+                    FieldVariables\getTheme(),
+                    FieldVariables\getNavStyle(),
+                ]
+            ]
+        ]
+    ];
+}
