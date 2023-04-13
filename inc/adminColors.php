009f768896aa35b580e8181922ac066273505955
Arne Spremberg <mail@arnespremberg.com>
2023-04-13 19:41:45 +0200

    lint

--- inc/adminColors.php
+++ inc/adminColors.php
@@ -1,8 +1,9 @@
 <?php
 
-add_filter( 'get_user_option_admin_color', 'update_user_option_admin_color', 5 );
+add_filter('get_user_option_admin_color', 'update_user_option_admin_color', 5);
 
-function update_user_option_admin_color( $color_scheme ) {
+function update_user_option_admin_color($color_scheme)
+{
     $color_scheme = 'modern';
 
     return $color_scheme;
