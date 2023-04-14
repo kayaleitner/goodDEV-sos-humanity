237a3c4786619787324f1dd3a2d965235e0a44fe
Arne Spremberg <mail@arnespremberg.com>
2023-04-14 14:21:45 +0200

    update disabel comments

--- inc/disableComments.php
+++ inc/disableComments.php
@@ -1,23 +1,41 @@
 <?php
 
-// Removes from admin menu
-add_action('admin_menu', 'my_remove_admin_menus');
-function my_remove_admin_menus()
-{
+add_action('admin_init', function () {
+    // Redirect any user trying to access comments page
+    global $pagenow;
+     
+    if ($pagenow === 'edit-comments.php') {
+        wp_safe_redirect(admin_url());
+        exit;
+    }
+ 
+    // Remove comments metabox from dashboard
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+ 
+    // Disable support for comments and trackbacks in post types
+    foreach (get_post_types() as $post_type) {
+        if (post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
+});
+ 
+// Close comments on the front-end
+add_filter('comments_open', '__return_false', 20, 2);
+add_filter('pings_open', '__return_false', 20, 2);
+ 
+// Hide existing comments
+add_filter('comments_array', '__return_empty_array', 10, 2);
+ 
+// Remove comments page in menu
+add_action('admin_menu', function () {
     remove_menu_page('edit-comments.php');
-}
-// Removes from post and pages
-add_action('init', 'remove_comment_support', 100);
-
-function remove_comment_support()
-{
-    remove_post_type_support('post', 'comments');
-    remove_post_type_support('page', 'comments');
-}
-// Removes from admin bar
-function mytheme_admin_bar_render()
-{
-    global $wp_admin_bar;
-    $wp_admin_bar->remove_menu('comments');
-}
-add_action('wp_before_admin_bar_render', 'mytheme_admin_bar_render');
+});
+ 
+// Remove comments links from admin bar
+add_action('init', function () {
+    if (is_admin_bar_showing()) {
+        remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+    }
+});
