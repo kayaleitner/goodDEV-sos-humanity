0dba3ecbd8acd0e822ab20715a4e33077e5fa59a
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-04-17 15:46:26 +0200

    lint

--- inc/disableRestApi.php
+++ inc/disableRestApi.php
@@ -1,6 +1,7 @@
 <?php
 
 add_filter('rest_authentication_errors', 'disable_rest_api');
-function disable_rest_api($access) {
-  return new WP_Error('rest_disabled', __('The REST API on this site has been disabled.'), array('status' => rest_authorization_required_code()));
+function disable_rest_api($access)
+{
+    return new WP_Error('rest_disabled', __('The REST API on this site has been disabled.'), array('status' => rest_authorization_required_code()));
 }
