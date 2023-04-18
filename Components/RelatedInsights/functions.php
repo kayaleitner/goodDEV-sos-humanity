8ef47bbfa30ce84a87e455805f0c10eb10c0797d
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-04-18 16:48:48 +0200

    add related insights (relevanssi)

---
+++ Components/RelatedInsights/functions.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Flynt\Components\RelatedInsights;
+
+use Flynt\FieldVariables;
+use Flynt\Utils\Options;
+use Timber\Timber;
+
+add_filter('Flynt/addComponentData?name=RelatedInsights', function ($data) {
+
+    $related_posts = relevanssi_get_related_post_ids($post_id);
+    $data['related_posts'] = $related_posts;
+
+    $data['posts'] = Timber::get_posts([
+        'post_status' => 'publish',
+        'post_type' => 'post',
+        'posts_per_page' => 3,
+        'ignore_sticky_posts' => 1,
+        'post__in' => $data['related_posts']
+    ]);
+
+    return $data;
+});
+
+function getACFLayout()
+{
+    return [
+        'name' => 'RelatedInsights',
+        'label' => __('Related Insights', 'flynt'),
+        'sub_fields' => [
+            [
+                'label' => __('General', 'flynt'),
+                'name' => 'generalTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => __('Title', 'flynt'),
+                'instructions' => __('Want to add a headline? And a paragraph? Go ahead! Or just leave it empty and nothing will be shown.', 'flynt'),
+                'name' => 'blockTitle',
+                'type' => 'text',
+            ],
+        ]
+    ];
+}
+
+Options::addTranslatable('RelatedInsights', [
+    [
+        'label' => __('Labels', 'flynt'),
+        'name' => 'labelsTab',
+        'type' => 'tab',
+        'placement' => 'top',
+        'endpoint' => 0
+    ],
+    [
+        'label' => '',
+        'name' => 'labels',
+        'type' => 'group',
+        'sub_fields' => [
+            [
+                'label' => __('All Posts', 'flynt'),
+                'name' => 'allPosts',
+                'type' => 'text',
+                'default_value' => __('View all', 'flynt'),
+                'required' => 0,
+                'wrapper' => [
+                    'width' => 50
+                ],
+            ],
+            [
+                'label' => __('All Posts Link', 'flynt'),
+                'name' => 'allPostsLink',
+                'type' => 'link',
+                'required' => 0,
+                'wrapper' => [
+                    'width' => 50
+                ],
+            ],
+            [
+                'label' => __('Read More', 'flynt'),
+                'name' => 'readMore',
+                'type' => 'text',
+                'default_value' => __('Read More', 'flynt'),
+                'required' => 0,
+                'wrapper' => [
+                    'width' => 50
+                ],
+            ],
+            [
+                'label' => __('Reading Time - (20) min read', 'flynt'),
+                'instructions' => __('%d is placeholder for number of minutes', 'flynt'),
+                'name' => 'readingTime',
+                'type' => 'text',
+                'default_value' => __('%d min read', 'flynt'),
+                'required' => 0,
+                'wrapper' => [
+                    'width' => 50
+                ],
+            ]
+        ],
+    ]
+]);
