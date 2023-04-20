bceaad6af7d8eee36c1192f31eeb417ebb7624bd
Anton Leitner <hello@antonleitner.dev>
2023-04-20 15:49:25 +0200

    mapListing projects opacity fade and loading from start
    # Conflicts:
    #	Components/ListingMap/index.twig

--- Components/ListingMap/script.js
+++ Components/ListingMap/script.js
@@ -6,6 +6,14 @@ const resolution = 10
 export default function (listingMap) {
   const refs = buildRefs(listingMap)
   const wrapper = refs.mapWrapper
+  const mapAndProjects = refs.mapAndProjects
+
+  // find the project which is the highest and set the min height of the map to that
+  const projects = listingMap.getElementsByClassName('project');
+  const maxHeight = Math.max(...Array.from(projects).map(project => project.offsetHeight))
+  mapAndProjects.style.minHeight = maxHeight + 'px'
+
+  // TODO: on resize, update the height of the map
 
   const map = new DottedMap({
     height: wrapper.offsetHeight / resolution,
