33c855cde53d71d2eea9d778c6d7461a6b486568
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-05-05 15:00:14 +0200

    lint

--- Components/BlockNewsletter/functions.php
+++ Components/BlockNewsletter/functions.php
@@ -9,38 +9,6 @@ use Flynt\ComponentManager;
 use Flynt\FieldVariables;
 use Timber\Timber;
 
-// function getACFLayout()
-// {
-//     return [
-//         'name' => 'BlockNewsletter',
-//         'label' => __('Block: Newsletter', 'flynt'),
-//         'sub_fields' => [
-//             [
-//                 'label' => __('General', 'flynt'),
-//                 'name' => 'generalTab',
-//                 'type' => 'tab',
-//                 'placement' => 'top',
-//                 'endpoint' => 0,
-//             ],
-//             [
-//                 'label' => __('Content', 'flynt'),
-//                 'name' => 'contentHtml',
-//                 'type' => 'wysiwyg',
-//                 'delay' => 1,
-//                 'media_upload' => 0,
-//                 'required' => 0,
-//             ],
-//             [
-//                 'label' => __('Options', 'flynt'),
-//                 'name' => 'optionsTab',
-//                 'type' => 'tab',
-//                 'placement' => 'top',
-//                 'endpoint' => 0
-//             ],
-//         ]
-//     ];
-// }
-
 Options::addTranslatable('BlockNewsletter', [
     [
         'label' => __('General', 'flynt'),
