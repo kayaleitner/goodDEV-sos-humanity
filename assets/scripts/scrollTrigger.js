8301ac8fdcca68bf4a1218fce8827ee9ece222cc
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-05-08 16:51:13 +0200

    lint

--- assets/scripts/scrollTrigger.js
+++ assets/scripts/scrollTrigger.js
@@ -1,15 +1,15 @@
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 export default () => ({
-    removeBackdropBlur() {
-        const mainNavigation = document.querySelector('.mainNavBlock');
-        ScrollTrigger.getAll().find(trigger => trigger.isActive).trigger.dataset?.navstyle?.includes('blur') && mainNavigation.classList.remove('backdrop-blur-xl');
-    },
-    addBackdropBlur() {
-        const mainNavigation = document.querySelector('.mainNavBlock');
-        ScrollTrigger.getAll().find(trigger => trigger.isActive).trigger.dataset?.navstyle?.includes('blur') && mainNavigation.classList.add('backdrop-blur-xl');
-    }
+  removeBackdropBlur () {
+    const mainNavigation = document.querySelector('.mainNavBlock')
+    ScrollTrigger.getAll().find(trigger => trigger.isActive).trigger.dataset?.navstyle?.includes('blur') && mainNavigation.classList.remove('backdrop-blur-xl')
+  },
+  addBackdropBlur () {
+    const mainNavigation = document.querySelector('.mainNavBlock')
+    ScrollTrigger.getAll().find(trigger => trigger.isActive).trigger.dataset?.navstyle?.includes('blur') && mainNavigation.classList.add('backdrop-blur-xl')
+  }
 })
