adae12d73ecb988f401e8770201d1009ac2b4148
Anton Leitner <hello@antonleitner.dev>
2023-05-11 14:26:39 +0200

    linting

--- Components/HeroIllustrationLottie/script.js
+++ Components/HeroIllustrationLottie/script.js
@@ -3,56 +3,56 @@ import { create } from '@lottiefiles/lottie-interactivity'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 
 export default function (heroIllustrationLottie) {
-    const players = heroIllustrationLottie.querySelectorAll('lottie-player')
-    Array.from(players).forEach(player => {
-        const animationType = player.dataset.animationtype
-        // switch case for animationType
-        switch (animationType) {
-            case 'playOnce':
-                player.setAttribute('autoplay', true)
-                return
+  const players = heroIllustrationLottie.querySelectorAll('lottie-player')
+  Array.from(players).forEach(player => {
+    const animationType = player.dataset.animationtype
+    // switch case for animationType
+    switch (animationType) {
+      case 'playOnce':
+        player.setAttribute('autoplay', true)
+        return
 
-            case 'loop':
-                // create({
-                //     mode: 'scroll',
-                //     player: player,
-                //     actions: [
-                //         {
-                //             type: 'loop',
-                //             visibility: [0, 1],
-                //             frames: [0, 100],
-                //         }
-                //     ]
-                // })
-                player.setAttribute('autoplay', true)
-                player.setAttribute('loop', true)
-                return
+      case 'loop':
+        // create({
+        //     mode: 'scroll',
+        //     player: player,
+        //     actions: [
+        //         {
+        //             type: 'loop',
+        //             visibility: [0, 1],
+        //             frames: [0, 100],
+        //         }
+        //     ]
+        // })
+        player.setAttribute('autoplay', true)
+        player.setAttribute('loop', true)
+        return
 
-            case 'scrubbed':
-                fetch(player.src) // eslint-diable-line
-                    .then(response => response.json())
-                    .then(data => {
-                        // Get the number of frames in the animation
-                        const numFrames = data.op - data.ip
-                        create({
-                            mode: 'scroll',
-                            player,
-                            actions: [
-                                {
-                                    type: 'seek',
-                                    visibility: [0, 1],
-                                    frames: [0, numFrames]
-                                }
-                            ]
-                        })
-                        ScrollTrigger.refresh()
-                    })
-                return
-            default:
-        }
-    })
+      case 'scrubbed':
+        fetch(player.src) // eslint-diable-line
+          .then(response => response.json())
+          .then(data => {
+            // Get the number of frames in the animation
+            const numFrames = data.op - data.ip
+            create({
+              mode: 'scroll',
+              player,
+              actions: [
+                {
+                  type: 'seek',
+                  visibility: [0, 1],
+                  frames: [0, numFrames]
+                }
+              ]
+            })
+            ScrollTrigger.refresh()
+          })
+        return
+      default:
+    }
+  })
 
-    ScrollTrigger.refresh()
+  ScrollTrigger.refresh()
 }
 
 // Autoplay on scroll --. not working
