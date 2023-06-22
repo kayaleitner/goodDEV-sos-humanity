316dcb8eb5ca0efc1c978933a5b2b5aa15dc335b
Anton Leitner <hello@antonleitner.dev>
2023-06-22 14:25:45 +0200

    remove postcss autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -19,9 +19,6 @@ export default defineConfig(({ mode }) => {
           importer: globImporter()
         }
       },
-      postcss: {
-        plugins: [autoprefixer()]
-      }
     },
     resolve: {
       alias: {
