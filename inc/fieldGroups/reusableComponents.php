8c9839a1bc6ceed2c0db794233f1d3586e61e36e
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-07-19 14:27:41 +0200

    add HeroContactForm to Fixed Blocks

--- inc/fieldGroups/reusableComponents.php
+++ inc/fieldGroups/reusableComponents.php
@@ -21,6 +21,7 @@ add_action('Flynt/afterRegisterComponents', function () {
                     Components\BlockVideoOembed\getACFLayout(),
                     Components\BlockWysiwyg\getACFLayout(),
                     Components\BlockGridCards\getACFLayout(),
+                    Components\HeroContactForm\getACFLayout(),
                     Components\ListingInsights\getACFLayout(),
                     Components\ListingProjects\getACFLayout(),
                     Components\ListComponents\getACFLayout(),
