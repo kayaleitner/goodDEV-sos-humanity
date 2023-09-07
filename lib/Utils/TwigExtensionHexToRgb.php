cf9848294ca8f0fec20086bd43bacda7155d5aab
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-09-07 16:40:41 +0200

    update coloring system

---
+++ lib/Utils/TwigExtensionHexToRgb.php
@@ -0,0 +1,35 @@
+<?php 
+
+namespace Flynt\Utils;
+
+use Twig\TwigFilter;
+use Twig\Extension\AbstractExtension;
+
+class TwigExtensionHexToRgb extends AbstractExtension
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('hex_to_rgb', [$this, 'hexToRgb']),
+        ];
+    }
+
+    public function hexToRgb($hexColor)
+    {
+        // Remove '#' if it exists
+        $hexColor = ltrim($hexColor, '#');
+
+        // If the color is in shorthand, expand it first
+        if (strlen($hexColor) == 3) {
+            $hexColor = $hexColor[0] . $hexColor[0] . $hexColor[1] . $hexColor[1] . $hexColor[2] . $hexColor[2];
+        }
+
+        $int = hexdec($hexColor);
+
+        return [
+            'red'   => ($int >> 16) & 0xFF,
+            'green' => ($int >> 8) & 0xFF,
+            'blue'  => $int & 0xFF
+        ];
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,137 +1,99 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    '*.php',
-    'templates/**/*.{php,twig}',
-    './Components/**/*.{php,twig}',
-    './Atoms/**/*.{php,twig}'
-  ],
-  theme: {
+export const content = [
+  '*.php',
+  'templates/**/*.{php,twig}',
+  './Components/**/*.{php,twig}',
+  './Atoms/**/*.{php,twig}',
+]
+export const theme = {
+  borderWidth: {
+    DEFAULT: '2px',
+    0: '0',
+    1: '1px',
+    2: '2px',
+    3: '3px',
+  },
+  colors: {
+    /** Colors (Primitives) */
+    current: 'currentColor',
+    transparent: 'transparent',
+    /** Colors (Tokens) */
+    textColor: 'var(--textColor)',
+    bgColor: 'var(--bgColor)',
+    brandColor: 'var(--brandColor)',
+    hoverColor: 'var(--hoverColor)',
+    // accentColor: 'var(--accentColor)',
+    // borderColor: 'var(--borderColor)',
+    // uiColor: 'var(--uiColor)',
+  },
+  fontSize: {
+    body: ['1.563rem', '140%'],
+    bodyMobile: ['1.25rem', 'normal'],
+    button: ['1.25rem', 'normal'],
+    buttonSmall: ['1.125rem', 'normal'],
+    titleLarge: ['4.813rem', 'normal'],
+    titleLargeMobile: ['2.813rem', 'normal'],
+    titleSmall: ['3.438rem', '110%'],
+    titleSmallMobile: ['2.188rem', 'normal'],
+    subtitle: ['2.188rem', 'normal'],
+    subtitleMobile: ['1.875rem', 'normal'],
+    glance: ['2.813rem', '110%'],
+    glanceMobile: ['1.5rem', '110%'],
+    caption: ['1.125rem', 'normal'],
+    captionMobile: ['0.938rem', 'normal'],
+  },
+  screens: {
+    xs: '640px',
+    sm: '780px',
+    md: '980px',
+    lg: '1080px',
+    xl: '1280px',
+    max: '1440px',
+  },
+  extend: {
+    aspectRatio: {
+      '4/3': '4 / 3',
+      '3/4': '3 / 4',
+      '2/1': '2 / 1',
+    },
     borderWidth: {
       DEFAULT: '2px',
       0: '0',
       1: '1px',
       2: '2px',
-      3: '3px'
+      3: '3px',
+      4: '4px',
     },
-    colors: {
-      /** Colors (Primitives) */
-      current: 'currentColor',
-      transparent: 'transparent',
-      /** Colors (Tokens) */
-      textColor: 'var(--textColor)',
-      bgColor: 'var(--bgColor)',
-      brandColor: 'var(--brandColor)',
-      accentColor: 'var(--accentColor)',
-      hoverColor: 'var(--hoverColor)',
-      borderColor: 'var(--borderColor)',
-      uiColor: 'var(--uiColor)',
-      errorColor: 'var(--errorColor)',
-      dangerColor: 'var(--dangerColor)',
+    brightness: {
+      70: '.70',
     },
-    fontSize: {
-      body: ['1.563rem', '140%'],
-      bodyMobile: ['1.25rem', 'normal'],
-      button: ['1.25rem', 'normal'],
-      buttonSmall: ['1.125rem', 'normal'],
-      titleLarge: ['4.813rem', 'normal'],
-      titleLargeMobile: ['2.813rem', 'normal'],
-      titleSmall: ['3.438rem', '110%'],
-      titleSmallMobile: ['2.188rem', 'normal'],
-      subtitle: ['2.188rem', 'normal'],
-      subtitleMobile: ['1.875rem', 'normal'],
-      glance: ['2.813rem', '110%'],
-      glanceMobile: ['1.5rem', '110%'],
-      caption: ['1.125rem', 'normal'],
-      captionMobile: ['0.938rem', 'normal']
+    fontFamily: {
+      mono: ['Aeonik Mono'],
     },
-    screens: {
-      xs: '640px',
-      sm: '780px',
-      md: '1024px',
-      lg: '1280px',
-      xl: '1440px',
-      max: '1600px'
+    height: {
+      xs: '10px',
+      sm: '15px',
+      md: '35px',
+      lg: '50px',
+      xl: '75px',
+      xxl: '125px',
+      max: '200px',
     },
-    extend: {
-      aspectRatio: {
-        '4/3': '4 / 3',
-        '3/4': '3 / 4',
-        '2/1': '2 / 1'
-      },
-      borderWidth: {
-        DEFAULT: '2px',
-        0: '0',
-        1: '1px',
-        2: '2px',
-        3: '3px',
-        4: '4px'
-      },
-      brightness: {
-        70: '.70'
-      },
-      fontFamily: {
-        mono: ['Aeonik Mono']
-      },
-      height: {
-        xs: '10px',
-        sm: '15px',
-        md: '35px',
-        lg: '50px',
-        xl: '75px',
-        xxl: '125px',
-        max: '200px'
-      },
-      spacing: {
-        xs: '10px',
-        sm: '18px',
-        md: '35px',
-        lg: '50px',
-        xl: '75px',
-        xxl: '120px',
-        max: '200px'
-      },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: 'currentColor',
-            fontSize: '1.25rem',
-            lineHeight: 'normal',
-            a: {
-              color: 'currentColor',
-              '&:hover': {
-                color: 'currentColor'
-              }
-            },
-            strong: {
-              color: 'currentColor',
-              '&:hover': {
-                color: 'currentColor'
-              }
-            },
-            hr: {
-              borderColor: 'currentColor',
-              marginBottom: '1.3em',
-              marginTop: '0'
-            }
-          }
-        },
-        lg: {
-          css: {
-            color: 'currentColor',
-            fontSize: '1.563rem',
-            lineHeight: '140%',
-            hr: {
-              borderColor: 'currentColor',
-              marginBottom: '1.3em',
-              marginTop: '0'
-            }
-          }
-        }
-      })
-    }
+    spacing: {
+      xs: '10px',
+      sm: '18px',
+      md: '35px',
+      lg: '50px',
+      xl: '75px',
+      xxl: '120px',
+      max: '200px',
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography')
-  ]
 }
+export const purge = {
+  safelist: ['w-1/2', 'translate-x-full', '!translate-x-0'],
+}
+
+export const plugins = []
+
+export const darkMode = 'class'
