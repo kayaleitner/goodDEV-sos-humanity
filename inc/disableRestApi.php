b4c2848487bee13c36bd4a122de733d05fb3d682
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2023-09-07 14:17:11 +0200

    enable rest api for yoast

--- inc/disableRestApi.php
+++ inc/disableRestApi.php
@@ -8,6 +8,11 @@ function disable_rest_api($access) {
         return $access;
     }
 
+    // Allow access to Yoast SEO REST API endpoints (adjust if Yoast uses different endpoints in the future)
+    if (strpos($_SERVER['REQUEST_URI'], '/wp-json/yoast/v1/') !== false) {
+        return $access;
+    }
+
     // Deny access to all other REST API routes
     return new WP_Error('rest_disabled', __('The REST API on this site has been disabled.'), ['status' => rest_authorization_required_code()]);
 }
