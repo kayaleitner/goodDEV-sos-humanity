c9c6e9009a1f05168f4282b84bdba9bb27e78965
Arne Spremberg <mail@arnespremberg.com>
2023-11-21 23:29:39 +0100

    feat (global colors): proper color loading for ACF color selector !main

--- inc/colorPalette.php
+++ inc/colorPalette.php
@@ -1,16 +1,35 @@
 <?php
 
-function theme_add_color_palette()
-{
+use Flynt\Utils\Options;
 
-    add_theme_support(
-        'editor-color-palette',
-        '#000',
-        '#fff',
-        '#169b83',
-        '#0c2569',
-        '#52b756',
-        '#6ff69d',
-    );
+function parseColorPalette()
+{
+    $cssContent = file_get_contents(get_template_directory() . '/assets/styles/_colors.css');
+    $parser = new \Sabberworm\CSS\Parser($cssContent);
+    $cssDocument = $parser->parse();
+    $colors = array();
+    $theme = get_option('options_globalThemeOption');
+
+    foreach ($cssDocument->getAllDeclarationBlocks() as $block) {
+        foreach ($block->getSelectors() as $selector) {
+            if ($selector->getSelector() === ':root' || $selector->getSelector() === "[data-theme='$theme']") {
+                // exit(var_dump($block->getRules()));
+                foreach ($block->getRules() as $rule) {
+                    $outputFormat = \Sabberworm\CSS\OutputFormat::create();
+                    $slug = str_replace('-', '', $rule->getRule());
+                    if (str_contains($rule->getValue()->render($outputFormat), '#')) {
+                        $colors[] = [
+                            'name' => $slug,
+                            'slug' => $slug,
+                            'color' => $rule->getValue()->render(\Sabberworm\CSS\OutputFormat::create()),
+                        ];
+                    }
+                }
+            }
+        }
+    }
+
+    return $colors;
 }
-add_action('init', 'theme_add_color_palette');
+
+add_theme_support('editor-color-palette', parseColorPalette());
