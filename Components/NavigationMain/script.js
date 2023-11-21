6fefb88639806168c83f319a9f503a8b9f899062
Julian Aliabadi <julian@aliabadi.de>
2023-11-21 14:04:53 +0100

    refactor(navigationMain): disable section scrollTrigger

--- Components/NavigationMain/script.js
+++ Components/NavigationMain/script.js
@@ -13,13 +13,13 @@ export default function (el) {
 
   onBreakpointChange()
 
-  function onBreakpointChange () {
+  function onBreakpointChange() {
     if (isDesktopMediaQuery.matches) {
       setScrollPaddingTop()
     }
   }
 
-  function setScrollPaddingTop () {
+  function setScrollPaddingTop() {
     const scrollPaddingTop = document.getElementById('wpadminbar')
       ? navigationHeight + document.getElementById('wpadminbar').offsetHeight
       : navigationHeight
@@ -29,67 +29,67 @@ export default function (el) {
   const sections = gsap.utils.toArray([
     '#mainContent flynt-component',
     '.single-post #mainContent article',
-    '.single-people #mainContent article'
+    '.single-people #mainContent article',
   ])
-  sections.forEach((section) => {
-    gsap.to(section, {
-      scrollTrigger: {
-        trigger: section,
-        start: 'top-=135 top',
-        end: 'bottom top+=70',
-        onToggle: (self) => {
-          if (self.isActive) {
-            const button = el.querySelector('#ctaMenu')
+  // sections.forEach((section) => {
+  //   gsap.to(section, {
+  //     scrollTrigger: {
+  //       trigger: section,
+  //       start: 'top-=135 top',
+  //       end: 'bottom top+=70',
+  //       onToggle: (self) => {
+  //         if (self.isActive) {
+  //           const button = el.querySelector('#ctaMenu')
 
-            el.querySelectorAll('.logo').forEach((logo) =>
-              logo.classList.remove('flex', 'hidden')
-            )
-            el.classList.remove(
-              'bg-bgColor/50',
-              'backdrop-blur-xl',
-              'text-textColor',
-              'text-bgColor'
-            )
-            button.classList.remove(
-              '[&_a]:bg-accentColor',
-              '[&_a]:hover:bg-brandColor',
-              '[&_a]:hover:text-bgColor',
-              '[&_a]:bg-bgColor',
-              '[&_a]:hover:bg-accentColor'
-            )
+  //           el.querySelectorAll('.logo').forEach((logo) =>
+  //             logo.classList.remove('flex', 'hidden')
+  //           )
+  //           el.classList.remove(
+  //             'bg-bgColor/50',
+  //             'backdrop-blur-xl',
+  //             'text-textColor',
+  //             'text-bgColor'
+  //           )
+  //           button.classList.remove(
+  //             '[&_a]:bg-accentColor',
+  //             '[&_a]:hover:bg-brandColor',
+  //             '[&_a]:hover:text-bgColor',
+  //             '[&_a]:bg-bgColor',
+  //             '[&_a]:hover:bg-accentColor'
+  //           )
 
-            self.trigger.dataset?.navstyle?.includes('blur') &&
-              el.classList.add('backdrop-blur-xl')
+  //           self.trigger.dataset?.navstyle?.includes('blur') &&
+  //             el.classList.add('backdrop-blur-xl')
 
-            self.trigger.dataset?.navstyle?.includes('dark')
-              ? el.querySelector('.logo_dark').classList.add('hidden')
-              : el.querySelector('.logo_dark').classList.add('flex')
-            !self.trigger.dataset?.navstyle?.includes('dark')
-              ? el.querySelector('.logo_light').classList.add('hidden')
-              : el.querySelector('.logo_light').classList.add('flex')
+  //           self.trigger.dataset?.navstyle?.includes('dark')
+  //             ? el.querySelector('.logo_dark').classList.add('hidden')
+  //             : el.querySelector('.logo_dark').classList.add('flex')
+  //           !self.trigger.dataset?.navstyle?.includes('dark')
+  //             ? el.querySelector('.logo_light').classList.add('hidden')
+  //             : el.querySelector('.logo_light').classList.add('flex')
 
-            self.trigger.dataset?.navstyle?.includes('dark')
-              ? el.classList.add('text-bgColor')
-              : el.classList.add('bg-bgColor/50', 'text-textColor')
-            self.trigger.dataset?.navstyle?.includes('dark')
-              ? button.classList.add('[&_a]:bg-bgColor', '[&_a]:hover:bg-accentColor')
-              : button.classList.add(
-                '[&_a]:bg-accentColor',
-                '[&_a]:hover:bg-brandColor',
-                '[&_a]:hover:text-bgColor'
-              )
-          }
-        }
-      }
-    })
-  })
+  //           self.trigger.dataset?.navstyle?.includes('dark')
+  //             ? el.classList.add('text-bgColor')
+  //             : el.classList.add('bg-bgColor/50', 'text-textColor')
+  //           self.trigger.dataset?.navstyle?.includes('dark')
+  //             ? button.classList.add('[&_a]:bg-bgColor', '[&_a]:hover:bg-accentColor')
+  //             : button.classList.add(
+  //               '[&_a]:bg-accentColor',
+  //               '[&_a]:hover:bg-brandColor',
+  //               '[&_a]:hover:text-bgColor'
+  //             )
+  //         }
+  //       }
+  //     }
+  //   })
+  // })
 
   // hide/show navigation on scroll
   const showAnim = gsap
     .from('[name="NavigationMain"]', {
       yPercent: -100,
       paused: true,
-      duration: 0.4
+      duration: 0.4,
       // scrub: 0.5,
     })
     .progress(1)
@@ -119,14 +119,14 @@ export default function (el) {
           element.scrollIntoView()
         }
       }
-    }
+    },
   })
   // hide/show cta on scroll
   const showCtaAnim = gsap
     .from('#ctaMain', {
       yPercent: 300,
       paused: true,
-      duration: 0.4
+      duration: 0.4,
       // scrub: 0.5,
     })
     .progress(1)
@@ -136,7 +136,7 @@ export default function (el) {
     endTrigger: '.pageWrapper',
     onUpdate: (self) => {
       self.direction === -1 ? showCtaAnim.play() : showCtaAnim.reverse()
-    }
+    },
   })
 }
 
