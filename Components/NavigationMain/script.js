dc060be8679cb09f47af1e99de7dac15c2d3e8fc
Julian Aliabadi <julian@aliabadi.de>
2023-11-21 16:15:19 +0100

    fix(navigationMain): add styles and scroll-behavior

--- Components/NavigationMain/script.js
+++ Components/NavigationMain/script.js
@@ -1,8 +1,60 @@
-import gsap from 'gsap'
-import ScrollTrigger from 'gsap/ScrollTrigger'
+// import gsap from 'gsap'
+// import ScrollTrigger from 'gsap/ScrollTrigger'
 import $ from 'jquery'
+import { buildRefs } from '@/assets/scripts/helpers.js'
 
 export default function (el) {
+  const refs = buildRefs(el)
+  initNavState()
+
+  function initNavState() {
+    // const prevScrollPos = $(window).scrollTop()
+    const currentScrollPos = $(window).scrollTop()
+
+    if (currentScrollPos > 0) {
+      setScrolledState()
+    } else {
+      unsetScrolledState()
+    }
+  }
+
+  function setScrolledState() {
+    refs.nav.classList.remove('text-bgColor')
+    refs.nav.classList.add('bg-bgColor')
+    refs.logo.classList.add('hidden')
+    refs.logoDark.classList.remove('hidden')
+    $('.button--outlineWhite', refs.nav)
+      .removeClass('button--outlineWhite')
+      .addClass('button--outlineBlack')
+    $('.button--icon', refs.nav)
+      .removeClass('button--icon')
+      .addClass('button--iconBlack')
+  }
+
+  function unsetScrolledState() {
+    refs.nav.classList.remove('bg-bgColor')
+    refs.nav.classList.add('text-bgColor')
+    refs.logoDark.classList.add('hidden')
+    refs.logo.classList.remove('hidden')
+    $('.button--outlineBlack', refs.nav)
+      .removeClass('button--outlineBlack')
+      .addClass('button--outlineWhite')
+    $('.button--iconBlack', refs.nav)
+      .removeClass('button--iconBlack')
+      .addClass('button--icon')
+  }
+
+  function handleScroll() {
+    const currentScrollPos = $(window).scrollTop()
+    if (currentScrollPos > 0) {
+      setScrolledState()
+    } else {
+      unsetScrolledState()
+    }
+  }
+
+  $(window).on('scroll', handleScroll)
+
   const navigationHeight =
     parseInt(
       window.getComputedStyle(el).getPropertyValue('--navigation-height')
@@ -26,11 +78,11 @@ export default function (el) {
     document.documentElement.style.scrollPaddingTop = `${scrollPaddingTop}px`
   }
 
-  const sections = gsap.utils.toArray([
-    '#mainContent flynt-component',
-    '.single-post #mainContent article',
-    '.single-people #mainContent article',
-  ])
+  // const sections = gsap.utils.toArray([
+  //   '#mainContent flynt-component',
+  //   '.single-post #mainContent article',
+  //   '.single-people #mainContent article',
+  // ])
   // sections.forEach((section) => {
   //   gsap.to(section, {
   //     scrollTrigger: {
@@ -85,70 +137,70 @@ export default function (el) {
   // })
 
   // hide/show navigation on scroll
-  const showAnim = gsap
-    .from('[name="NavigationMain"]', {
-      yPercent: -100,
-      paused: true,
-      duration: 0.4,
-      // scrub: 0.5,
-    })
-    .progress(1)
+  // const showAnim = gsap
+  //   .from('[name="NavigationMain"]', {
+  //     yPercent: -100,
+  //     paused: true,
+  //     duration: 0.4,
+  //     // scrub: 0.5,
+  //   })
+  //   .progress(1)
 
-  ScrollTrigger.create({
-    start: 'center top-=100',
-    endTrigger: '.pageWrapper',
-    end: 'bottom bottom',
-    onUpdate: (self) => {
-      const submenuWrapper = document.querySelector('.submenu-wrapper')
-      if (self.direction === -1 && !submenuWrapper.classList.contains('open')) {
-        showAnim.play()
-      } else if (
-        self.direction === 1 &&
-        !submenuWrapper.classList.contains('open')
-      ) {
-        showAnim.reverse()
-      }
-    },
-    onRefresh: (self) => {
-      // scroll to element id on load
-      const url = window.location.href
-      const hash = url.substring(url.indexOf('#') + 1)
-      if (hash) {
-        const element = document.getElementById(hash)
-        if (element) {
-          element.scrollIntoView()
-        }
-      }
-    },
-  })
-  // hide/show cta on scroll
-  const showCtaAnim = gsap
-    .from('#ctaMain', {
-      yPercent: 300,
-      paused: true,
-      duration: 0.4,
-      // scrub: 0.5,
-    })
-    .progress(1)
-  ScrollTrigger.create({
-    start: 'center top-=100',
-    end: 'bottom bottom',
-    endTrigger: '.pageWrapper',
-    onUpdate: (self) => {
-      self.direction === -1 ? showCtaAnim.play() : showCtaAnim.reverse()
-    },
-  })
+  // ScrollTrigger.create({
+  //   start: 'center top-=100',
+  //   endTrigger: '.pageWrapper',
+  //   end: 'bottom bottom',
+  //   onUpdate: (self) => {
+  //     const submenuWrapper = document.querySelector('.submenu-wrapper')
+  //     if (self.direction === -1 && !submenuWrapper.classList.contains('open')) {
+  //       showAnim.play()
+  //     } else if (
+  //       self.direction === 1 &&
+  //       !submenuWrapper.classList.contains('open')
+  //     ) {
+  //       showAnim.reverse()
+  //     }
+  //   },
+  //   onRefresh: (self) => {
+  //     // scroll to element id on load
+  //     const url = window.location.href
+  //     const hash = url.substring(url.indexOf('#') + 1)
+  //     if (hash) {
+  //       const element = document.getElementById(hash)
+  //       if (element) {
+  //         element.scrollIntoView()
+  //       }
+  //     }
+  //   },
+  // })
+  // // hide/show cta on scroll
+  // const showCtaAnim = gsap
+  //   .from('#ctaMain', {
+  //     yPercent: 300,
+  //     paused: true,
+  //     duration: 0.4,
+  //     // scrub: 0.5,
+  //   })
+  //   .progress(1)
+  // ScrollTrigger.create({
+  //   start: 'center top-=100',
+  //   end: 'bottom bottom',
+  //   endTrigger: '.pageWrapper',
+  //   onUpdate: (self) => {
+  //     self.direction === -1 ? showCtaAnim.play() : showCtaAnim.reverse()
+  //   },
+  // })
 }
 
-$(document).ready(() => {
-  $('.submenuItemLink').on('click', () => {
-    $('.mainNavBlock').removeClass('backdrop-blur-xl')
-    $('.menu').removeClass('text-bgColor')
-    $('.submenu-wrapper').removeClass('open')
-    $('.submenu-wrapper').css('display', 'none')
-    $('.tab-control').removeClass('open')
-    $('.tab-control').removeClass('underline')
-    $('#blur-overlay').css('display', 'none')
-    $('body').css('overflow', 'auto')
-  })
-})
+// $(document).ready(() => {
+//   $('.submenuItemLink').on('click', () => {
+//     $('.mainNavBlock').removeClass('backdrop-blur-xl')
+//     $('.menu').removeClass('text-bgColor')
+//     $('.submenu-wrapper').removeClass('open')
+//     $('.submenu-wrapper').css('display', 'none')
+//     $('.tab-control').removeClass('open')
+//     $('.tab-control').removeClass('underline')
+//     $('#blur-overlay').css('display', 'none')
+//     $('body').css('overflow', 'auto')
+//   })
+// })
