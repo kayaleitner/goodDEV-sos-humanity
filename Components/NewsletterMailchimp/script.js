b6b7b0cd5165728496e324de56fa149ca82dcea5
Julian Aliabadi <julian@aliabadi.de>
2023-12-14 15:35:43 +0100

    fix(newsletterMailchimp): add frontend and js functions to open the panel

--- Components/NewsletterMailchimp/script.js
+++ Components/NewsletterMailchimp/script.js
@@ -1,74 +1,87 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 import $ from 'jquery'
 import { buildRefs } from '@/assets/scripts/helpers.js'
 
+export function open() {
+  // Hacky way of getting the element
+  const el = document.querySelector('flynt-component[name=NewsletterMailchimp]')
+  const refs = buildRefs(el)
+  el.setAttribute('data-open', true)
+  disableBodyScroll(refs.formContainer)
+}
+
+export function close() {
+  // Hacky way of getting the element
+  const el = document.querySelector('flynt-component[name=NewsletterMailchimp]')
+  const refs = buildRefs(el)
+  el.setAttribute('data-open', false)
+  enableBodyScroll(refs.formContainer)
+}
+
 export default (el) => {
   const refs = buildRefs(el)
+  setEventListeners(refs)
 
-  // eslint-disable-next-line no-undef
-  const { myAjaxVar } = FlyntData // FlyntData is set in inc/assets.php
-
-  refs.form.noValidate = true
-  $(refs.form)
-    .find('input')
-    .each((index, elem) => {
-      // console.log(elem)
-      elem.addEventListener('input', () => {
-        if (elem.checkValidity()) {
-          $(elem).prev('label').removeClass('hasError')
-        } else {
-          $(elem).prev('label').addClass('hasError')
-        }
-      })
+  function setEventListeners() {
+    $(refs.close).on('click', () => {
+      close()
     })
-
-  $(refs.form).on('submit', (e) => {
-    console.log('submit!')
-    e.preventDefault()
-    console.log($(refs.form).serializeArray())
-
+    // eslint-disable-next-line no-undef
+    const { myAjaxVar } = FlyntData // FlyntData is set in inc/assets.php
+    refs.form.noValidate = true
     $(refs.form)
       .find('input')
       .each((index, elem) => {
-        if (elem.checkValidity()) {
-          $(elem).prev('label').removeClass('hasError')
-        } else {
-          console.log($(elem).attr('name'), 'failed')
-          $(elem).prev('label').addClass('hasError')
-        }
+        elem.addEventListener('input', () => {
+          if (elem.checkValidity()) {
+            $(elem).prev('label').removeClass('hasError')
+          } else {
+            $(elem).prev('label').addClass('hasError')
+          }
+        })
       })
 
-    if (!refs.form.checkValidity()) {
-      e.stopImmediatePropagation()
-      return
-    }
+    $(refs.form).on('submit', (e) => {
+      e.preventDefault()
 
-    // eslint-disable-next-line prefer-const
-    let theData = {
-      action: 'subscribe_to_mailchimp_list',
-      nonce: myAjaxVar.nonce,
-      payload: {},
-    }
+      $(refs.form)
+        .find('input')
+        .each((index, elem) => {
+          if (elem.checkValidity()) {
+            $(elem).prev('label').removeClass('hasError')
+          } else {
+            $(elem).prev('label').addClass('hasError')
+          }
+        })
 
-    $(refs.form)
-      .serializeArray()
-      .forEach((elem) => {
-        theData.payload[elem.name] = elem.value
+      if (!refs.form.checkValidity()) {
+        e.stopImmediatePropagation()
+        return
+      }
+
+      // eslint-disable-next-line prefer-const
+      let theData = {
+        action: 'subscribe_to_mailchimp_list',
+        nonce: myAjaxVar.nonce,
+        payload: {},
+      }
+
+      $(refs.form)
+        .serializeArray()
+        .forEach((elem) => {
+          theData.payload[elem.name] = elem.value
+        })
+
+      $.ajax({
+        type: 'POST',
+        url: myAjaxVar.ajaxUrl,
+        dataType: 'json',
+        data: theData,
+        success(response) {
+          const r = JSON.parse(JSON.stringify(response))
+          // TODO: handle response
+        },
       })
-
-    console.log(theData)
-
-    $.ajax({
-      type: 'POST',
-      url: myAjaxVar.ajaxUrl,
-      dataType: 'json',
-      data: theData,
-      success(response) {
-        console.log(response)
-        const r = JSON.parse(JSON.stringify(response))
-        console.log(r)
-
-        // TODO: handle response
-      },
     })
-  })
+  }
 }
