486d47c5de44a08decdffc913c83c7b64df99d34
Arne Spremberg <mail@arnespremberg.com>
2024-09-06 16:12:18 -0400

    chore(global): move unused components

--- inc/fieldGroups/reusableComponents.php
+++ inc/fieldGroups/reusableComponents.php
@@ -16,14 +16,11 @@ add_action('Flynt/afterRegisterComponents', function (): void {
                 'type' => 'flexible_content',
                 'button_label' => __('Add Block', 'flynt'),
                 'layouts' => [
-                    Components\BlockImage\getACFLayout(),
-                    Components\BlockImageText\getACFLayout(),
-                    Components\BlockSeparatorLine\getACFLayout(),
-                    Components\BlockSpacer\getACFLayout(),
-                    Components\BlockVideoOembed\getACFLayout(),
                     Components\BlockWysiwyg\getACFLayout(),
-                    Components\BlockHeroContactForm\getACFLayout(),
-                    Components\BlockSliderImages\getACFLayout(),
+                    Components\BlockImageText\getACFLayout(),
+                    Components\BlockCards\getACFLayout(),
+                    Components\BlockCarousel\getACFLayout(),
+                    Components\BlockStatistics\getACFLayout(),
                 ],
             ]
         ],
