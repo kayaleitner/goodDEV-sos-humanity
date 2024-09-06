b25ab3051202f792921c1b972b858c5052da2d9f
Arne Spremberg <mail@arnespremberg.com>
2024-09-06 16:07:50 -0400

    feat(styles): add postcss function to calculate rems

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,20 @@
+import postcssFunctions from 'postcss-functions';
+
 export default {
   plugins: {
     'postcss-import': {},
     'tailwindcss/nesting': {},
     'postcss-nested-ancestors': {},
-    'postcss-calc': {},
+    'postcss-functions': {
+      functions: {
+        rem: (value) => {
+          const number = parseFloat(value);
+          return `calc(${number} / 16 * 1rem)`;
+        },
+      },
+    },
+    'postcss-calc': {}, // This should come after postcss-functions
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
